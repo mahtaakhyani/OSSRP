f662eede913a7660a7472b2dff8496c9ae90f1eb
Mahta Akhyani <mahtaakhyani@gmail.com>
2023-10-10 10:31:08 +0000

    test

--- cleancoded/webInterface/interface_backend/core/static/js/script.js
+++ cleancoded/webInterface/interface_backend/core/static/js/script.js
@@ -37,7 +37,7 @@ function sleep (time) {
 // ---------------------------------
 var host = '192.168.43.250';
 var port = '5353';
-var android_port = '8080';
+var android_port = '5353';
 var face_url_id = '';
 var sound_url_val = '';
 var auto_imit_val = false;
