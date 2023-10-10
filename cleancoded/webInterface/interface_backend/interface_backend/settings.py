fe667a7a9c778251522e488afcee622735ecbea2
Mahta Akhyani <mahtaakhyani@gmail.com>
2023-10-10 07:04:18 +0000

    bugs fixed

--- cleancoded/webInterface/interface_backend/interface_backend/settings.py
+++ cleancoded/webInterface/interface_backend/interface_backend/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PARROT_DIR = os.path.join(BASE_DIR, "parrot_control")
+# PARROT_DIR = os.path.join(BASE_DIR, "serialHandler/parrot_control")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -89,7 +89,7 @@ TEMPLATES = [
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
-    os.path.join(BASE_DIR, 'parrot_control/templates'),
+    # os.path.join(BASE_DIR, 'serialHandler/parrot_control/templates'),
 )
 
 WSGI_APPLICATION = 'interface_backend.wsgi.application'
@@ -143,9 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(PARROT_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(PARROT_DIR, "static")
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
