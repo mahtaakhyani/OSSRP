a2ee0e9262819f34ad6c817d2461965f84c4f939
Mahta Akhyani <mahtaakhyani@gmail.com>
2023-12-15 22:48:37 +0000

    AI generated speech successful

--- cleancoded/src/infrastructure/scripts/services/ai/conversation_bot.py
+++ cleancoded/src/infrastructure/scripts/services/ai/conversation_bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
-from translate import Translator
+from deep_translator import GoogleTranslator
 import rospy
 from std_msgs.msg import String
 
@@ -16,7 +16,7 @@ class ConversationService:
     def callservice(self, data):
         rospy.loginfo('Got a conversation text on /say_sth/bot. Calling the bot module.')
         try:
-            response = self.bot.start_conversation(data)
+            response = self.bot.start_conversation(data.data)
             self.pub.publish(response)
             
         except rospy.ServiceException as e:
@@ -27,10 +27,12 @@ class ConversationService:
 
 
 class ConversationBot:
-    translator_fa= Translator(to_lang="en", from_lang="fa")
-    translator_en= Translator(to_lang="fa", from_lang="en")
+    translator_fa= GoogleTranslator(source='auto', target='fa')
+    translator_en= GoogleTranslator(source='auto', target='en')
     tokenizer = GPT2Tokenizer.from_pretrained("af1tang/personaGPT")
+    rospy.loginfo("Loading the model...")
     model = GPT2LMHeadModel.from_pretrained("af1tang/personaGPT")
+    rospy.loginfo("Model loaded successfully.")
     
     def __init__(self):
         if torch.cuda.is_available():
@@ -40,9 +42,10 @@ class ConversationBot:
 
         # get personality facts for conversation
         self.personas = []
-        for i in range(3):
-            response = input(">> Fact %d: "%(i+1))+ self.tokenizer.eos_token
-            self.personas.append(response)
+        # for i in range(3):
+        #     response = input(">> Fact %d: "%(i+1))+ self.tokenizer.eos_token
+        #     self.personas.append(response)
+        self.personas = ['I am a robot.', 'My name is Hooshang', 'I love kids.']
         self.personas = self.tokenizer.encode(''.join(['<|p2|>'] + self.personas + ['<|sep|>'] + ['<|start|>']))
 
 
@@ -76,7 +79,7 @@ class ConversationBot:
         return msg
 
     def start_conversation(self, input_fa):
-
+        rospy.loginfo("User: "+input_fa)
         # The first use of PersonaGPT is to do personalized dialog generation. Use the following loop to interact with the model.
         # converse for 8 turns
         dialog_hx = []
@@ -90,9 +93,9 @@ class ConversationBot:
         bot_input_ids = self.to_var([self.personas + self.flatten(dialog_hx)]).long()
         msg = self.generate_next(bot_input_ids)
         dialog_hx.append(msg)
-        response_en = "Bot: {}".format(self.tokenizer.decode(msg, skip_special_tokens=True))
+        response_en = self.tokenizer.decode(msg, skip_special_tokens=True)
         response_fa = self.translator_en.translate(response_en)
-        
+        rospy.loginfo("Bot: "+response_en)
         return response_fa
 
 
