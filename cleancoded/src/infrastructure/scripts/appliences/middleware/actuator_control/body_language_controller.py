67ebddbb5abeef4431d8fe920f6716354d57b6a9
Mahta Akhyani <mahtaakhyani@gmail.com>
2023-12-29 11:32:22 +0000

    .

--- cleancoded/src/infrastructure/scripts/appliences/middleware/actuator_control/body_language_controller.py
+++ cleancoded/src/infrastructure/scripts/appliences/middleware/actuator_control/body_language_controller.py
@@ -339,11 +339,11 @@ class FeelingsController:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Subscribe to a ROS topic to get the emotion data used in body language controller')
-    parser.add_argument('--topic_name', type=str, help='Name of the topic to subscribe to', required=False, default="/face_emotions")
+    parser.add_argument('--topic', type=str, help='Name of the topic to subscribe to', required=False, default="/face_emotions")
     args = parser.parse_args()
 
     try:
-        FeelingsController(args.topic_name)
+        FeelingsController(args.topic)
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
