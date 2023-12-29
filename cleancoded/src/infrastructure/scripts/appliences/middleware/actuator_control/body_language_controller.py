3d301fd317bc02d98f1f994237e8e19eef1cf653
Mahta Akhyani <mahtaakhyani@gmail.com>
2023-12-29 11:29:33 +0000

    .

--- cleancoded/src/infrastructure/scripts/appliences/middleware/actuator_control/body_language_controller.py
+++ cleancoded/src/infrastructure/scripts/appliences/middleware/actuator_control/body_language_controller.py
@@ -339,7 +339,7 @@ class FeelingsController:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Subscribe to a ROS topic to get the emotion data used in body language controller')
-    parser.add_argument('topic_name', type=str, default='/face_emotions', help='Name of the topic to subscribe to')
+    parser.add_argument('--topic_name', type=str,  nargs='?', help='Name of the topic to subscribe to')
     args = parser.parse_args()
 
     try:
